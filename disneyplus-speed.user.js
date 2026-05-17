591ae30f959271e651a934611e0b3137c5628589
Courtney Andrew Richardson <courtneyrichardson2027@u.northwestern.edu>
2026-05-17 00:33:57 -0500

    feat: add userscript skeleton with Tampermonkey header

---
+++ disneyplus-speed.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Disney+ Speed Controller
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Floating playback speed controls for Disney+
+// @author       You
+// @match        https://www.disneyplus.com/video/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+})();
